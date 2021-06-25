17b0badd5dd1bb82e5977ad31536d521a97e7a8d
Pete Matsyburka <pete.matsy@gmail.com>
2021-06-25 10:13:46 +0300

    fix AR filters on rails 6.1.4

--- lib/motor/active_record_utils/active_record_filter_patch.rb
+++ lib/motor/active_record_utils/active_record_filter_patch.rb
@@ -5,3 +5,14 @@ ActiveRecord::Filter.module_eval do
     @filters ||= HashWithIndifferentAccess.new
   end
 end
+
+if Rails::VERSION::MAJOR == 6
+  ActiveRecord::Relation::Filter.module_eval do
+    def build_arel(aliases = nil)
+      arel = super
+      my_alias_tracker = ActiveRecord::Associations::AliasTracker.create(connection, table.name, [])
+      build_filters(arel, my_alias_tracker)
+      arel
+    end
+  end
+end
