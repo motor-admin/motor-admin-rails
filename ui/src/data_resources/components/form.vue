8c3415b2b704cab3f52216235f0fc95263fcca6e
Pete Matsyburka <pete.matsy@gmail.com>
2021-07-03 15:25:58 +0300

    (#22) reset save and create data on success

--- ui/src/data_resources/components/form.vue
+++ ui/src/data_resources/components/form.vue
@@ -195,6 +195,10 @@ export default {
       try {
         await this.apiRequest
 
+        if (eventData.button === 'save_and_create') {
+          this.resourceData = this.normalizeResourceData(this.resource)
+        }
+
         this.$emit('success', eventData)
       } catch (error) {
         if (error.response?.data?.errors) {
@@ -207,10 +211,6 @@ export default {
       } finally {
         this.isSaveAndNewLoading = false
         this.isSaveLoading = false
-
-        if (eventData.button === 'save_and_create') {
-          this.resourceData = this.normalizeResourceData(this.resource)
-        }
       }
     }
   }
