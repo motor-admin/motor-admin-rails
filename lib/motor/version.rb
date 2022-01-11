849aeba3dffc7326265f657be538754f34f10502
Pete Matsyburka <pete.matsy@gmail.com>
2022-01-11 13:21:34 +0200

    fix column format

--- lib/motor/version.rb
+++ lib/motor/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Motor
-  VERSION = '0.2.53'
+  VERSION = '0.2.54'
 end
--- ui/src/data_forms/components/input.vue
+++ ui/src/data_forms/components/input.vue
@@ -40,9 +40,9 @@
     :model-value="dataValue"
     :options="tagOptions"
     :allow-create="!tagOptions.length"
-    :multiple="column.is_array || !!column.format.split_tags_by"
+    :multiple="column.is_array || !!column.format?.split_tags_by"
     :label-function="tagOptions.length ? (option) => titleize(option.value.toString()) : (option) => option.value.toString()"
-    @update:modelValue="$emit('update:modelValue', column.format.split_tags_by ? $event.join(column.format.split_tags_by) : $event)"
+    @update:modelValue="$emit('update:modelValue', column.format?.split_tags_by ? $event.join(column.format.split_tags_by) : $event)"
     @select="onSelect"
   />
   <OptionsInput
@@ -181,7 +181,7 @@ export default {
         this.dataValue = new Date(new Date(this.modelValue).getTime())
       } else if (this.isDate) {
         this.dataValue = new Date(new Date(this.modelValue).getTime() + new Date().getTimezoneOffset() * 60000)
-      } else if (this.isTagSelect && this.column.format.split_tags_by) {
+      } else if (this.isTagSelect && this.column.format?.split_tags_by) {
         this.dataValue = this.modelValue.split(this.column.format.split_tags_by).map((e) => e.trim())
       }
     }
