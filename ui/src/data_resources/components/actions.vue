79f0a3b7bc2a11e2683184813cec7253bdc4b397
Pete Matsyburka <pete.matsy@gmail.com>
2023-06-17 11:10:13 +0300

    show new actions added without page refresh

--- ui/src/data_resources/components/actions.vue
+++ ui/src/data_resources/components/actions.vue
@@ -239,7 +239,7 @@ export default {
     },
     permittedActions () {
       return (this.actions || this.model.actions).filter((action) => {
-        return action.apply_on === 'member' && action.visible && this.resources.some((res) => {
+        return (!action.apply_on || action.apply_on === 'member') && action.visible && this.resources.some((res) => {
           return this.$can(action.name, this.model.class_name, res)
         })
       })
