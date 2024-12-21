af6f2d62607dd255c1a7aa796583187e3a3ced00
Pete Matsyburka <pete.matsy@gmail.com>
2024-12-21 23:37:49 +0200

    fix rails 8

--- lib/motor/queries/postgresql_exec_query.rb
+++ lib/motor/queries/postgresql_exec_query.rb
@@ -6,21 +6,30 @@ module Motor
       module_function
 
       def call(conn, statement)
-        conn.send(:execute_and_clear, *statement) do |result|
-          types = {}
-          fields = result.fields
-
-          fields.each_with_index do |fname, i|
-            ftype = result.ftype i
-            fmod  = result.fmod i
-            types[fname] = conn.send(:get_oid_type, ftype, fmod, fname)
+        if Rails.version.to_f >= 8.0
+          result = conn.send(:internal_execute, *statement)
+          process_result(conn, result)
+        else
+          conn.send(:execute_and_clear, *statement) do |result|
+            process_result(conn, result)
           end
+        end
+      end
 
-          if conn.respond_to?(:build_result, true)
-            conn.send(:build_result, columns: fields, rows: result.values, column_types: types)
-          else
-            ActiveRecord::Result.new(fields, result.values, types)
-          end
+      def process_result(conn, result)
+        types = {}
+        fields = result.fields
+
+        fields.each_with_index do |fname, i|
+          ftype = result.ftype i
+          fmod  = result.fmod i
+          types[fname] = conn.send(:get_oid_type, ftype, fmod, fname)
+        end
+
+        if conn.respond_to?(:build_result, true)
+          conn.send(:build_result, columns: fields, rows: result.values, column_types: types)
+        else
+          ActiveRecord::Result.new(fields, result.values, types)
         end
       end
     end
--- lib/motor/version.rb
+++ lib/motor/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Motor
-  VERSION = '0.4.32'
+  VERSION = '0.4.33'
 end
